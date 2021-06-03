82bcdb0d101c1ebc371b31604d5285cb6730ddab
pataman3 <bryan@bryanbean.com>
2021-06-03 16:27:43 -0400

    Fix user model

--- models/user.js
+++ models/user.js
@@ -1,17 +1,34 @@
 const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = new Sequelize("database", "username", "password", {
   dialect: "mysql",
+  freezeTableName: true,
 });
 
 const User = sequelize.define(
   "user",
   {
-    email: {
+    first_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
     password: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("csr"),
       allowNull: false,
     },
   },

79fb33bc618371538615b1161fb78d1e32076441
Joseph Blake <jos09461@tjx.com>
2021-06-03 12:07:49 -0400

    checking interaction of orders(Home)(Controller)

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -4,6 +4,9 @@ var ordersController={
     orderHome(req,res){
       
       /*mock order for now* TEST*/
-      orders = {id:1,cost:500,items:4}
+      orders = [{id:1,cost:500,items:4},{id:2,cost:600,items:5}]
       res.render('ordersHome', orders)
+      }
 }
+
+module.exports = ordersController;

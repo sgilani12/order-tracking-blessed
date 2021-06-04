51332f7b2dc0862d61d804decd34d80f17716a38
pataman3 <bryan@bryanbean.com>
2021-06-04 08:51:45 -0400

    Fix user model again

--- models/user.js
+++ models/user.js
@@ -24,7 +24,7 @@ const User = sequelize.define(
       },
     },
     user_password: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(1000),
       allowNull: false,
     },
     user_role: {

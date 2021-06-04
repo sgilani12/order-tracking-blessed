f5399d6e60364e355ab31ee384a2715a3a7173ed
Isabel Tonelli-Sippel <isabelTSmusic@outlook.com>
2021-06-04 12:18:36 -0400

    add navbar css file

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use('/orders', ordersRouter);
 app.use('/customers', customersRouter);
 app.use('/newOrder', newOrderRouter);
 
+app.use(express.static(path.join(__dirname, "static")));
+
 app.listen(port, () => {
     console.log(`Server listening on Port: ${port}`);
 });

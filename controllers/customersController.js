aeecdb0625fd4d9ddb3801ffdbc4ca931ab324b4
Caroline Sekel <40600716+carolinesekel@users.noreply.github.com>
2021-06-07 11:30:59 -0400

    list of customer data being displayed in html table, sample object in place until backend code is merged

--- controllers/customersController.js
+++ controllers/customersController.js
@@ -1,8 +1,3 @@
-const axios = require('axios');
-//import axios from 'axios';
-
-//const fetch = require("node-fetch");
-
 var customersController={
 
   customerHome(req,res){
@@ -13,39 +8,6 @@ var customersController={
     res.render('newCustomer')
     }
 }
-
-
-/*db.query(sql, function (err, rows) {
-  if (err) {
-
-      var message = err.message;
-      console.log(message);
-
-      return res.status(500).send(message);
-
-  }else {
-
-      var data = rows;
-      res.render('history/search-history', {data: data});
-
-  }
-});*/
-
-/*fetch('http://localhost:3000/customers')
-.then(data => {
-  console.log(data.json());
- return data.json();
-}).then(data =>{
-  console.log(data);
-  return data;
-})
-.catch(err=>{
-  console.log(err);
-})*/
-axios.get('/customers').then(resp => {
-
-    console.log(resp.data);
-});
 
 
 

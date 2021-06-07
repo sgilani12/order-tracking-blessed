656d80ff15a37d8192cbf7057feff7b50d2d212d
Caroline Sekel <40600716+carolinesekel@users.noreply.github.com>
2021-06-07 10:41:59 -0400

    some axios progress

--- controllers/customersController.js
+++ controllers/customersController.js
@@ -1,3 +1,8 @@
+const axios = require('axios');
+//import axios from 'axios';
+
+//const fetch = require("node-fetch");
+
 var customersController={
 
   customerHome(req,res){
@@ -8,5 +13,40 @@ var customersController={
     res.render('newCustomer')
     }
 }
+
+
+/*db.query(sql, function (err, rows) {
+  if (err) {
+
+      var message = err.message;
+      console.log(message);
+
+      return res.status(500).send(message);
+
+  }else {
+
+      var data = rows;
+      res.render('history/search-history', {data: data});
+
+  }
+});*/
+
+/*fetch('http://localhost:3000/customers')
+.then(data => {
+  console.log(data.json());
+ return data.json();
+}).then(data =>{
+  console.log(data);
+  return data;
+})
+.catch(err=>{
+  console.log(err);
+})*/
+axios.get('/customers').then(resp => {
+
+    console.log(resp.data);
+});
+
+
 
 module.exports = customersController;

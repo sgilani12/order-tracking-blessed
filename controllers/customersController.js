b2df338330da974223aeecdaee39ca887f61b6d1
pataman3 <bryan@bryanbean.com>
2021-06-08 16:04:14 -0400

    Add products GET route

--- controllers/customersController.js
+++ controllers/customersController.js
@@ -1,67 +1,61 @@
 var customerModel = require("../models/customer");
 
-
-var customersController={
-    customerHome(req, res) {
-        // get customers from db as list of objects
-        customerModel.getCustomerList((err,data)=>{
-            try {
-                if(err) {
-                    console.log(err)
-                } else {
-                   res.render('customers', {customers:data});
-                }
-            }
-            catch (error) {
-                console.log(error)
-            }
-        });
-    },
-  
-    customerNew(req,res){
-      res.render('newCustomer')
-    },
-    addCustomer(req, res) {
-        // how to handle optional fields?
-        console.log(req.body)
-        try {
-            // unpack req.body and create customer object
-            const customer = {
-                first_name:req.body.fname,
-                middle_name:req.body.mname,
-                last_name:req.body.lname,
-                phone:req.body.phone,
-                email:req.body.email,
-                customer_notes:req.body.notes,
-                shipping_address:req.body.shipaddress,
-                billing_address:req.body.billaddress
-            }
-            // add/save user to db
-            // re-direct to customerHome
-    
-            // this method calls the function in customerModel.js to save to db
-            customerModel.addCustomer(customer, (foundCustomer, created) => {
-                if (created) {
-                    res.redirect("/customers");
-                } else {
-                    console.log("CUSTOMER EXISTS ", foundCustomer);
-                    res.redirect("/customers/add");
-                }
-            });
+var customersController = {
+  customerHome(req, res) {
+    // get customers from db as list of objects
+    customerModel.getCustomerList((err, data) => {
+      try {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("customers", { customers: data });
         }
-       catch (error) {
+      } catch (error) {
         console.log(error);
       }
-    },
+    });
+  },
 
   customerNew(req, res) {
     res.render("newCustomer");
   },
+  addCustomer(req, res) {
+    // how to handle optional fields?
+    console.log(req.body);
+    try {
+      // unpack req.body and create customer object
+      const customer = {
+        first_name: req.body.fname,
+        middle_name: req.body.mname,
+        last_name: req.body.lname,
+        phone: req.body.phone,
+        email: req.body.email,
+        customer_notes: req.body.notes,
+        shipping_address: req.body.shipaddress,
+        billing_address: req.body.billaddress,
+      };
+      // add/save user to db
+      // re-direct to customerHome
+
+      // this method calls the function in customerModel.js to save to db
+      customerModel.addCustomer(customer, (foundCustomer, created) => {
+        if (created) {
+          res.redirect("/customers");
+        } else {
+          console.log("CUSTOMER EXISTS ", foundCustomer);
+          res.redirect("/customers/add");
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
   customerDelete(req, res) {
     res.render("deleteCustomer");
   },
   deleteCustomer(req, res) {
-    console.log("---------TEST-----------")
+    console.log("---------TEST-----------");
     try {
       const id = req.body.customerid;
       customerModel.deleteCustomer(id, (err, deleted) => {
@@ -82,6 +76,5 @@ var customersController={
     }
   },
 };
-
 
 module.exports = customersController;
--- routes/customers.js
+++ routes/customers.js
@@ -1,28 +1,32 @@
-const express = require('express');
+const express = require("express");
 
 const customersRouter = express.Router();
-const customersController = require('../controllers/customersController')
+const customersController = require("../controllers/customersController");
 
-customersRouter.route('/delete')
-    .post(customersController.deleteCustomer)
-    .get(customersController.customerDelete)
+customersRouter
+  .route("/delete")
+  .post(customersController.deleteCustomer)
+  .get(customersController.customerDelete);
 
-customersRouter.route('/')
-    .get(customersController.customerHome)
-    .post(customersController.addCustomer)
+customersRouter
+  .route("/")
+  .get(customersController.customerHome)
+  .post(customersController.addCustomer);
 
-customersRouter.route('/add')
-    .get(customersController.customerNew)   
+customersRouter
+  .route("/add")
+  .get(customersController.customerNew);
 
-customersRouter.route('/:id')
-    .get((req, res) => {
-        res.send(`Request: GET /customers/${req.params['id']}`);
-    })
-    .delete((req, res) => {
-        res.send(`Request: DELETE /customers/${req.params['id']}`); 
-    }) 
-    .put((req, res) => {
-        res.send(`Request: PUT /customers/${req.params['id']}`);
-    });
+customersRouter
+  .route("/:id")
+  .get((req, res) => {
+    res.send(`Request: GET /customers/${req.params["id"]}`);
+  })
+  .delete((req, res) => {
+    res.send(`Request: DELETE /customers/${req.params["id"]}`);
+  })
+  .put((req, res) => {
+    res.send(`Request: PUT /customers/${req.params["id"]}`);
+  });
 
 module.exports = customersRouter;

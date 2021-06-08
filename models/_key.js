cb1e0a9870d9437884386803a63bcea697045cd0
Joseph Blake <jos09461@tjx.com>
2021-06-08 12:06:32 -0400

    connected to azure db

--- models/_key.js
+++ models/_key.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
-exports.sequelize = new Sequelize("capstone_db", "root", "Welcome01", {
-  host: "localhost",
+exports.sequelize = new Sequelize("capstone_db", "CapstoneAdmins@capstone-db-server", "qwertyTJX321", {
+  host: "capstone-db-server.mysql.database.azure.com",
   dialect: "mysql",
   freezeTableName: true,
 });

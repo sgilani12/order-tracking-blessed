53ec9421e669cf9521718f02464373ea948f6b7f
Joseph Blake <jos09461@tjx.com>
2021-06-09 15:30:54 -0400

    removing references to bad auth

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,23 +1,11 @@
 var userModel = require('../models/user');
-var genToken = require('../passport');
 
 var loginController = {
   loginHome(req, res){
     res.render('login')
-  },
-
-  authenticateUser(req,res){
-    const {email, password} = req.body;
-    const user = User.findByPk(email);
-
-    if(user && user.authenticateUser(email, password)){
-      res.redirect('/dashboard').json(genToken(user))
-    }
-    else{
-      res.status(401).json({"message" : "Invalid credentials"})
-    }
-
   }
+
+ 
 }
 
 module.exports = loginController;

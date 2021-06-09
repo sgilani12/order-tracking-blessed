adfff6a4935c70a7e85eac37964b7397cb21b55c
joeblake <joe.blake8424@gmail.com>
2021-06-09 15:08:05 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const passport = require ('passport')
 
 var indexRouter = express.Router();
 
-indexRouter.get('/', passport.authenticate('jwt',{session: false}),function(req, res) {
-    res.render('index');
-});
+indexRouter.get('/',(req, res) => {
+    res.render('index')
+}
 
 
 module.exports = indexRouter;

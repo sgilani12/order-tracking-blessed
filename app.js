54c957ddea99f3fa4ff3a537869ce2010ba69184
apopesc <83656545+apopesc@users.noreply.github.com>
2021-06-09 15:46:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const genToken = require('./passport')
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 
 const urlParser = bodyparser.urlencoded({extended:true});

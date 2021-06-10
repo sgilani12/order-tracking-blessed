43338caf98179cfd59085070a83c831449ab06cc
Steve Desktop <stevesohn.ss@gmail.com>
2021-06-10 12:31:58 -0400

    fix customers order spec file

--- controllers/customersController.spec.js
+++ controllers/customersController.spec.js
@@ -5,15 +5,15 @@ const expect = require('chai').expect;
 const customerModel = require("../models/customer");
 
 describe('customer model', () => {
-  
+
     let fname;
     before((done) => {
         const mockCustomer = {
             first_name:"barry",
             middle_name:"tjx",
             last_name:"smith",
-            phone:"17774441131",
-            email:"122@tjx.com",
+            phone:"7774441131",
+            email:"22@tjx.com",
             customer_notes:"please work",
             shipping_address:"100 tjx rd",
             billing_address:"100 tjx rd"
@@ -24,89 +24,9 @@ describe('customer model', () => {
         });
     });
 
-    describe('Add customer', () => {
-        it("should ADD a customer correctly", () => {
-
-            const customer = {
-                first_name:"charlie",
-                middle_name:"leroy",
-                last_name:"trainor",
-                phone:"9789789978",
-                email:"jason_trainor@tjx.com",
-                customer_notes:"req.body.notes",
-                shipping_address:"req.body.shipaddress",
-                billing_address:"req.body.billaddress"
-            }
-
-            const customer2 = {
-                first_name:"charlie2",
-                middle_name:"leroy",
-                last_name:"trainor",
-                phone:"9789789978",
-                email:"jason_trainor@tjx.com",
-                customer_notes:"req.body.notes",
-                shipping_address:"req.body.shipaddress",
-                billing_address:"req.body.billaddress"
-            }
-
-            
-            customerModel.addCustomer(customer, (err, created)=> {
-                if (err) {
-                  var errorArray = err.errors;
-                  var messageArray = new Array();
-                  errorArray.forEach(e => {
-                    messageArray.push(e.message);
-                  });
-                  req.session['message'] = messageArray;
-                  console.log(req.session['message']);
-                  res.redirect('/customers/add');
-                } else {
-                  if (created) {
-                    req.session['message'] = ["Customer successfully created"];
-                    res.redirect('/customers');
-                  } else {
-                    req.session['message'] = ["An Error Has Occured"];
-                    res.redirect('/customers/add');
-                  }
-                }
-              })
-
-              customerModel.addCustomer(customer, (err, created)=> {
-                if (err) {
-                  var errorArray = err.errors;
-                  var messageArray = new Array();
-                  errorArray.forEach(e => {
-                    messageArray.push(e.message);
-                  });
-                  req.session['message'] = messageArray;
-                  console.log(req.session['message']);
-                  res.redirect('/customers/add');
-                } else {
-                  if (created) {
-                    req.session['message'] = ["Customer successfully created"];
-                    res.redirect('/customers');
-                  } else {
-                    req.session['message'] = ["An Error Has Occured"];
-                    res.redirect('/customers/add');
-                  }
-                }
-              })
-
-            customerModel.getCustomerList((err,data)=> {
-                try {
-                    if(err) {
-                        console.log(err)
-                    } else {
-                       expect(data).to.be.an('array');
-                       expect(data).to.have.length(30)
-                    }
-                }
-                catch (error) {
-                    console.log(error)
-                }
-            });
+    describe('GET add', () => {
+        it("should add a new customer", () => {
+            expect(fname).to.eq("henry");
         });
     });
-
-
 });
--- controllers/ordersController.spec.js
+++ controllers/ordersController.spec.js
@@ -6,27 +6,5 @@ const customerModel = require("../models/order");
 
 describe('customer order', () => {
 
-    let fname;
-    before((done) => {
-        const mockCustomer = {
-            first_name:"barry",
-            middle_name:"tjx",
-            last_name:"smith",
-            phone:"7774441131",
-            email:"22@tjx.com",
-            customer_notes:"please work",
-            shipping_address:"100 tjx rd",
-            billing_address:"100 tjx rd"
-        }
-        customerModel.addCustomer(mockCustomer, (err, created) => {
-            fname = created.dataValues.first_name;
-            done();
-        });
-    });
-
-    describe('GET add', () => {
-        it("should add a new customer", () => {
-            expect(fname).to.eq("henry");
-        });
-    });
+    
 });
